600b0cd4a3b2c04408e14067e15b1bc8a5d84f08
Heng Ly Aun <aunhengly@gmail.com>
2018-07-11 19:38:49 -0700

    inintial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        My name is Hengly
+      </div>
+    );
+  }
+}
+
+const Header = () => {
+  return <header></header>
+}
+
+export default App;
